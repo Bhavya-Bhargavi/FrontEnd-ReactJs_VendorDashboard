b9ed35a0e9486699109e4b466b494d7be65bced3
bhavya <robbi.bhavya@gmail.com>
2025-08-13 12:10:20 +0530

    First push fronend vendor dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/home' element= { <LandingPage />} />
+        <Route path='/' element= { <LandingPage />} />
         <Route path='/*' element={ <NotFound />} />
       </Routes>
     </div>
