6bf8385367dd2a14ca1d5563ec5496f38096b11c
bhavya <robbi.bhavya@gmail.com>
2025-08-13 12:25:30 +0530

    path change

--- src/vendorDashboard/helpers/ApiPath.js
+++ src/vendorDashboard/helpers/ApiPath.js
@@ -1,3 +1,3 @@
 //export const API_URL = "http://localhost:4000"; // in local use this
 
- export const API_URL = "https://backend-nodejs-hnek.onrender.com/";  // live uncomment and push this
+ export const API_URL = "https://backend-nodejs-hnek.onrender.com";  // live uncomment and push this
